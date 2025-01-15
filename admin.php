80952edd158e9ae6ccd1098988fcdefd2ad6f166
ondrej847 <ondra-schovanek@seznam.cz>
2025-01-15 23:17:13 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,3 +1,13 @@
+<?php
+/* kontrola sputeni session, jestli neni spustena tak ji my timto spustime */
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+require_once "header.php";
+require_once "footer.php"; 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
