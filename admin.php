ef2b1fb8b21fbcf7c296405c8090c4cb8fffa227
ondrej847 <ondra-schovanek@seznam.cz>
2025-01-25 15:57:16 +0100

    uprava css a html

--- admin.php
+++ admin.php
@@ -5,17 +5,11 @@ if (session_status() == PHP_SESSION_NONE) {
 }
 
 require_once "header.php";
-require_once "footer.php"; 
+require_once "funcs.php";
+
+$conn = connect_db();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+
+
+<?php require_once "footer.php"; ?>
--- ucet.php
+++ ucet.php
@@ -4,23 +4,10 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 require_once "header.php";
-require_once "footer.php";
+require_once "funcs.php";
 
+$conn = connect_db();
 
-
-
-$server = "localhost";
-$login = "root";
-$passwd = "";
-$schema = "eshop";
-
-
-$conn = new mysqli($server, $login, $passwd, $schema);
-
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 /* nacteni informaci z DB */
 $user_id = $_SESSION['user_id'];
 $sql = "SELECT * FROM uzivatele WHERE user_id = $user_id";
@@ -40,20 +27,27 @@ if ($result->num_rows > 0) {
     $role_id = $user['role_id']; /* chceme ziskat roli uzivatele */
 }
 $conn->close();
-
-
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Můj účet</title>
-    <link rel="stylesheet" href="style.css">
-</head>
+<style>
+    .account-info {
+        padding: 20px;
+        margin-top: 80px; /* Aby se to nepřekrývalo s hlavním menu */
+        background-color:rgb(236, 235, 235);
+        text-align: center;
+    }
 
-<body>
+    .account-details {
+        font-size: 18px;
+        color: #333;
+    }
+
+    .admin-button {
+        margin-top: 20px;
+        text-align: center;
+    }
+
+</style>
 
 <div class="account-info">
     <h2>Informace o účtu</h2>
@@ -71,56 +65,6 @@ $conn->close();
         </div>
     <?php endif; ?>
 </div>
-<style>
-body {
-    margin: 0px;
-    padding: 0px;
-}
-
-.account-info {
-    padding: 20px;
-    margin-top: 80px; /* Aby se to nepřekrývalo s hlavním menu */
-    background-color: #f4f4f4;
-    text-align: center;
-}
-
-.account-details {
-    font-size: 18px;
-    color: #333;
-}
-
-.admin-button {
-    margin-top: 20px;
-    text-decoration: none; 
-    color: rgb(136, 160, 141);
-    text-align: center;
-}
 
 
-/* zatim, TODO: pak opravit a odstranit */
-
-.buttons {
-    margin-right: 30px;  /* tlacitka si posunu trochu doprava */
-    display: flex;  /* aby byli tlacitka zarovnana vedle sebe */      
-    }
-
-.button {
-    padding: 10px 20px;  /* urcuje vnitrni okraje mezi obsahem a hranicemi */
-    margin-left: 15px;  /* mezera mezi tlacitky */ 
-    background-color:rgb(136, 160, 141);  /* pozadi tlacitek najito na internetu */
-    color: white;  /* Bílý text na tlačítkách */    
-    text-decoration: none;  /* aby nebyl text podtrzeny */
-    border-radius: 15px;  /* zaobleni rohu */
-
-    
-}
-
-.button:hover {
-    background-color: #45a049;  /* pri najeti mysi na tlacitka bude tmavsi zelena */
-} 
-
-
-</style>
-
-</body>
-</html>
+<?php require_once "footer.php"; ?>
